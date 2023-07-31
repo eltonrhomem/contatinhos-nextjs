cdc63c38da1aab17b5749e4e14432fd3e9413136
Elton Rodrigues Homem <eltonrhomem@gmail.com>
2023-07-30 21:26:05 -0300

    feat: salvar contatinhos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,80 @@
-import Image from 'next/image'
+"use client";
+import { Container, FormControl, FormControlLabel, FormLabel, Radio, Button, RadioGroup, Select, MenuItem } from "@mui/material";
+import Box from "@mui/material/Box";
+import { grey } from "@mui/material/colors";
+import Typography from '@mui/material/Typography';
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+
+interface Contatinho {
+  id: number,
+  nome: string,
+  genero: string,
+  signo: number,
+
+}
+
 
 export default function Home() {
+  const [contatinhos, setContatinhos] = useState<Contatinho[]>([])
+  const [contatinho, setContatinho] = useState<Contatinho>({id: 0, nome: '', genero: '', signo: 0})
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Container fixed>
+      <Box component="div" sx={{
+        width: '100%',
+        height: '100%',
+        margin: '10px',
+        padding: '10px',
+        backgroundColor: grey['200'],
+        // '&:hover': {
+        //   backgroundColor: 'primary.main',
+        //   opacity: [0.9, 0.8, 0.7],
+        // },
+      }} className={`rounded-lg `}>
+        <Typography variant="h3" className={`text-center`}>
+          Contatinhos
+        </Typography>
+        <Container className={`pt-4`}>
+        <FormControl className={`w-[100%] gap-3`}>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <TextField fullWidth label="Nome completo" onChange={(e) => setContatinho({...contatinho, nome: String(e.target.value)}) } />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <FormLabel id="demo-radio-buttons-group-label">Gênero</FormLabel>
+          <RadioGroup row defaultValue="M" onChange={(e) => setContatinho({...contatinho, genero: String(e.target.value) })}>
+            <FormControlLabel value="M" control={<Radio defaultChecked />} label="Masculino" />
+            <FormControlLabel value="F" control={<Radio />} label="Feminino" />
+          </RadioGroup>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <FormLabel>Signo</FormLabel>
+          <Select value={contatinho.signo} onChange={(e) => setContatinho({...contatinho, signo: Number( e.target.value) } ) }>
+            <MenuItem value={1}>Áries</MenuItem>
+            <MenuItem value={2}>Touro</MenuItem>
+            <MenuItem value={3}>Gêmeos</MenuItem>
+            <MenuItem value={4}>Câncer</MenuItem>
+            <MenuItem value={5}>Leão</MenuItem>
+            <MenuItem value={6}>Virgem</MenuItem>
+            <MenuItem value={7}>Libra</MenuItem>
+            <MenuItem value={8}>Escorpião</MenuItem>
+            <MenuItem value={9}>Sagitário</MenuItem>
+            <MenuItem value={10}>Capricórnio</MenuItem>
+            <MenuItem value={11}>Aquário</MenuItem>
+            <MenuItem value={12}>Peixes</MenuItem>
+          </Select>
+          
+        </FormControl>
+        <Container className="p-4">
+          <div>
+          <Button variant="contained" color="success" onClick={() => setContatinhos([...contatinhos, contatinho]) }>Salvar</Button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          </div>
+        </Container>
+          
+        </Container>
+      </Box>
+      
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </Container>
+  );
 }
